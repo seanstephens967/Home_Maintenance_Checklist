5eb82cefcf1cb7e7f5a96b92a0a21fb099d5e66b
seanstephens967 <seanstephens967@gmail.com>
2020-12-05 07:54:04 -0500

    adjusted webconfig

---
+++ HomeMaintenance.Data/Migrations/202012011236349_InitialCreate.cs
@@ -0,0 +1,153 @@
+﻿namespace HomeMaintenance.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Project",
+                c => new
+                    {
+                        ProjectID = c.Int(nullable: false, identity: true),
+                        ProjectName = c.String(nullable: false),
+                        ProjectTask = c.Int(nullable: false),
+                        Description = c.String(nullable: false),
+                        SuppliesNeeded = c.String(),
+                        Instructions = c.String(),
+                        EstimatedTime = c.String(),
+                        ProjectionCreateationDate = c.DateTimeOffset(nullable: false, precision: 7),
+                        ProjectCompletionDate = c.DateTimeOffset(precision: 7),
+                        TechnicianNotes = c.String(),
+                        TechnicianID = c.Int(nullable: false),
+                        PropertyID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProjectID)
+                .ForeignKey("dbo.Property", t => t.PropertyID)
+                .ForeignKey("dbo.Technician", t => t.TechnicianID)
+                .Index(t => t.TechnicianID)
+                .Index(t => t.PropertyID);
+            
+            CreateTable(
+                "dbo.Property",
+                c => new
+                    {
+                        PropertyID = c.Int(nullable: false, identity: true),
+                        PropertyOwnerFirstName = c.String(nullable: false),
+                        PropertyOwnerLastName = c.String(nullable: false),
+                        PropertyAddress = c.String(nullable: false),
+                        PropertyPhone = c.String(nullable: false),
+                        PropertyOwnerEmail = c.String(),
+                    })
+                .PrimaryKey(t => t.PropertyID);
+            
+            CreateTable(
+                "dbo.Technician",
+                c => new
+                    {
+                        TechnicianID = c.Int(nullable: false, identity: true),
+                        TechnicianName = c.String(nullable: false),
+                        TechnicianPhoneNumber = c.String(nullable: false),
+                        TechnicianEmail = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.TechnicianID);
+            
+            CreateTable(
+                "dbo.IdentityRole",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserRole",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(),
+                        IdentityRole_Id = c.String(maxLength: 128),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.IdentityRole", t => t.IdentityRole_Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.IdentityRole_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.ApplicationUser",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserClaim",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUserLogin",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(),
+                        ProviderKey = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.IdentityUserRole", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.Project", "TechnicianID", "dbo.Technician");
+            DropForeignKey("dbo.Project", "PropertyID", "dbo.Property");
+            DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.Project", new[] { "PropertyID" });
+            DropIndex("dbo.Project", new[] { "TechnicianID" });
+            DropTable("dbo.IdentityUserLogin");
+            DropTable("dbo.IdentityUserClaim");
+            DropTable("dbo.ApplicationUser");
+            DropTable("dbo.IdentityUserRole");
+            DropTable("dbo.IdentityRole");
+            DropTable("dbo.Technician");
+            DropTable("dbo.Property");
+            DropTable("dbo.Project");
+        }
+    }
+}
